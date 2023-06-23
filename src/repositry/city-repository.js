f2ce001a8c8c01de04a86512b1ce0a3f58198f22
himanshu kumar <hk7703940916@gmail.com>
2023-06-24 00:42:09 +0530

    Completed city repository

--- src/repositry/city-repository.js
+++ src/repositry/city-repository.js
@@ -1,27 +1,10 @@
 const {City} = require('../models/index');
 
 class CityRepository{
+   
+    async createCity(){}
 
-    async createCity({name}){
-        try{
-            const city = await City.create({name});
-            return city;
-        }catch(error){
-            throw {error};
-        }
-    }
-
-    async deleteCity( city_id ){
-        try{
-            await City.destroy({
-                where:{
-                    id:city_id
-                }
-            });
-        }catch(error){
-            throw({error});
-        }
-    }
+    
 }
 
 module.exports = CityRepository;
